d532da9f094d0bb916d01e5c9f9451faa4c5ab93
Joe Dolson <design@joedolson.com>
2024-03-13 19:34:30 -0500

    Wrap sequential accordion elements into containers & treat as discrete accordion groups

--- build/blocks/accordion/index.asset.php
+++ build/blocks/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6a939ead1add031a18b1');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '47cc7a3d2561d317a8e9');
